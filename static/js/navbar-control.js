ecf79a894b5f4874080332f81a2d474b5d93e6f3
Syaifudin Fendi Prasetyo <syaifudinfendip@gmail.com>
2024-01-20 22:24:32 +0700

    Navbar update

--- static/js/navbar-control.js
+++ static/js/navbar-control.js
@@ -35,3 +35,54 @@ contactactive = () => {
 //       let visibleElement = Math.floor(this.scrollY / 600);
 //       $(`#navbar${visibleElement + 1}`).addClass("active");
 //   });
+
+
+window.addEventListener('scroll', function() {
+    var windowHeight = window.innerHeight;
+    var aboutTarget = document.getElementById('main');
+    var aboutPosition = aboutTarget.getBoundingClientRect();
+    if (aboutPosition.top <= windowHeight) {
+        navAbout.addClass('active');
+        navSkill.removeClass('active');
+        navProject.removeClass('active');
+        navContact.removeClass('active');
+    } else {
+        navAbout.removeClass('active');
+    }
+
+    var windowHeight = window.innerHeight;
+    var skillTarget = document.getElementById('skill');
+    var skillPosition = skillTarget.getBoundingClientRect();
+    if (skillPosition.top <= windowHeight) {
+        navAbout.removeClass('active');
+        navSkill.addClass('active');
+        navProject.removeClass('active');
+        navContact.removeClass('active');
+    } else {
+        navSkill.removeClass('active');
+    }
+
+    var windowHeight = window.innerHeight;
+    var projectTarget = document.getElementById('project');
+    var projectPosition = projectTarget.getBoundingClientRect();
+    if (projectPosition.top <= windowHeight) {
+        navAbout.removeClass('active');
+        navSkill.removeClass('active');
+        navProject.addClass('active');
+        navContact.removeClass('active');
+    } else {
+        navProject.removeClass('active');
+    }
+
+    var windowHeight = window.innerHeight;
+    var contactTarget = document.getElementById('contact');
+    var contactPosition = contactTarget.getBoundingClientRect();
+    if (contactPosition.top <= windowHeight) {
+        navAbout.removeClass('active');
+        navSkill.removeClass('active');
+        navProject.removeClass('active');
+        navContact.addClass('active');
+    } else {
+        navContact.removeClass('active');
+    }
+});
